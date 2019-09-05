42c6da921b905f4d98c4c1a78c4912afb6680e98
Chandu <chandu.prince951@gmail.com>
2019-09-05 14:30:43 -0700

    Intial Commit

---
+++ HomeWork1/src/com/homework1/NewsLetterPublisher.kt
@@ -0,0 +1,9 @@
+package com.homework1
+
+object NewsLetterPublisher {
+    init {
+        println("Initializing with object: $this")
+    }
+
+    fun print() = println("Printing with object: $this")
+}
---
+++ HomeWork1/src/com/homework1/main.kt
@@ -0,0 +1,10 @@
+package com.homework1
+
+
+     fun test() {
+    val shark = NewsLetterPublisher.print()
+}
+    fun main()
+    {
+        test();
+    }
